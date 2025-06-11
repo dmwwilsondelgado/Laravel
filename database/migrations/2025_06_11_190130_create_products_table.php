1e76a88a61b856631b6475908589636c6b702ddd
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-11 15:07:43 -0500

    creacion de la relacion de productos

--- database/migrations/2025_06_11_190130_create_products_table.php
+++ database/migrations/2025_06_11_190130_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable(false);
+            $table->double('price')->nullable(false);
+            $table->integer('stock')->nullable(false);
+            //para relacionar 
+            $table->unsignedBigInteger('categorias_id')->nullable(false);
+            $table->foreign('categorias_id')->references('id')->on('categorias');
             $table->timestamps();
         });
     }

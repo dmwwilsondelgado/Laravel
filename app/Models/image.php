fd3a46008359642cd9b83bdee6af857a07e1e2a5
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-11 16:54:11 -0500

    agregamos cositas al models

--- app/Models/image.php
+++ app/Models/image.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 // $faker = Faker\Factory::create();
 // $faker->addProvider(new \Mmo\Faker\PicsumProvider($faker));
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class image extends Model
 {
-    //
+    use HasFactory;
 }
